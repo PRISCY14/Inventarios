b0a982268d7180a50d11787c606133949d682bc5
PRISCY14 <love_951214@hotmail.com>
2016-03-02 19:06:56 -0600

    primer trabajo

--- src/inventarios/Main.java
+++ src/inventarios/Main.java
@@ -15,7 +15,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+   
+        SaludosJFrame c = new SaludosJFrame();
+        c.setVisible(true);
+        
     }
 
 }
